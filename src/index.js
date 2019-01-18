c05cb89a894fcf9690c7b3308f2672e8165b9757
Inti <yeseniase@outlook.it>
2019-01-18 13:13:17 -0600

    Se agrega 1ra versión de proyecto terminado

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-const string = document.getElementById("message").value;
+let string = document.getElementById("message").value.toUpperCase();
 let offset = parseInt(document.getElementById("offset").value);
+//string = string.toUpperCase();
 
 function validarOffset(offset){
     console.log("validando: " + offset);
 	
 		if (offset === 0 || offset < 0){
-			alert("Se aplicará offset por defaut de 33");
+			//alert("Se aplicará offset por defaut de 33");
 			offset = 33;
-			console.log("Offset x default: " + offset);
+			//console.log("Offset x default: " + offset);
 			//window.cipher.encode(33,string);
 			//window.cipher.decode(33, string);
 			return offset;
 		} else {
-			alert("Se ha aplicado tu offset ;)")
-			console.log("offset dinámico xD");
+			alert("Se ha aplicado tu offset ;)");
+			//console.log("offset dinámico xD");
 			//return position;
 			return window.cipher.createCipherWithOffset(offset);
 		}
@@ -24,14 +25,14 @@ function validarOffset(offset){
 	container.innerHTML = msnOutput;
 }
 */
-validarOffset(offset);
+validarOffset(offset, string);
 
 let btnEncode = document.getElementById("btnEncode");
 btnEncode.addEventListener("click", function(){
 	//console.log("btnEncode");
-	let result = cipher.encode(offset,string);
+	let result = cipher.encode(offset,string);//***************** */
 	let container = document.getElementById("codeMsn");
-	let showMessage = document.getElementById("showMessage")
+	let showMessage = document.getElementById("showMessage");
 	showMessage.innerHTML = "Mensaje cifrado";
 	container.innerHTML = result;
 });
@@ -40,8 +41,8 @@ let btnDecode = document.getElementById("btnDecode");
 btnDecode.addEventListener("click", function(){
 	//console.log("btnDecode");
 	let container = document.getElementById("codeMsn");
-	let result = cipher.decode(offset,string);
-	let showMessage = document.getElementById("showMessage")
+	let result = cipher.decode(offset,string); //*********** */
+	let showMessage = document.getElementById("showMessage");
 	showMessage.innerHTML = "Mensaje descifrado";
 	container.innerHTML = result;
 });
