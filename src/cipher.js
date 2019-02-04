20477fb4cfb5640d967368adb51eb63316dc2b06
Inti <yeseniase@outlook.it>
2019-02-03 22:52:40 -0600

    Se refactoriza código JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,25 @@
 window.cipher = {
-  encode: function (offset, string) {
+	encode: (offset, string) => {
 		console.log("encode: " + offset);
-		
-		//string = string.toUpperCase();
 		let msnEncode = "";
-    console.log(string);
-    
-		for(let i = 0; i < string.length; i ++){
-      let numChar = ((string.charCodeAt(i) - 65 + offset) % 26) + 65;
-      
-			if(string[i] == " "){
+
+		for (let i = 0; i < string.length; i++) {
+			let numChar = ((string.charCodeAt(i) - 65 + offset) % 26) + 65;
+			if (string[i] == " ") {
 				msnEncode += string[i];
 			} else {
 				msnEncode += String.fromCharCode(numChar);
 			}
-
 		}
 		return msnEncode;
 	},
-  decode: function (offset,string) {
+	decode: (offset, string) => {
 		console.log("decode: " + offset);
-		//string = string.toUpperCase();
 		let msnDecode = "";
 
-		for(let i = 0; i<string.length; i++){
+		for (let i = 0; i < string.length; i++) {
 			let numChar = ((string.charCodeAt(i) + 65 - offset) % 26) + 65;
-			if(string[i] == " "){
+			if (string[i] == " ") {
 				msnDecode += string[i];
 			} else {
 				msnDecode += String.fromCharCode(numChar);
@@ -33,43 +27,33 @@ window.cipher = {
 		}
 		return msnDecode;
 	},
-	createCipherWithOffset: function (offset) {
-    //console.log("Offset: " + offset);
-    //console.log("string: " + window.document);
+	createCipherWithOffset: (offset) => {
 		let newObject = {
-			offset: offset,
-      //string: window.document.getElementById("message").value.toUpperCase(),
-      //string: window.document.all[7].value,
-			//encode: window.cipher.encode(offset, string),
-			//decode: window.cipher.decode(offset, string)
-			encode: function (string) {
+			offset: parseInt(offset),
+			encode: (string) => {
 				console.log("encode 2: " + newObject.offset);
 				offset = newObject.offset;
-				//string = string.toUpperCase();
 				let msnEncode = "";
-				//console.log(string);
-				
-				for(let i = 0; i < string.length; i ++){
+
+				for (let i = 0; i < string.length; i++) {
 					let numChar = ((string.charCodeAt(i) - 65 + offset) % 26) + 65;
-					
-					if(string[i] == " "){
+					if (string[i] == " ") {
 						msnEncode += string[i];
 					} else {
 						msnEncode += String.fromCharCode(numChar);
 					}
-		
+
 				}
 				return msnEncode;
 			},
-			decode: function (string) {
+			decode: (string) => {
 				console.log("decode 2: " + newObject.offset);
-				//string = string.toUpperCase();
 				offset = newObject.offset;
 				let msnDecode = "";
-		
-				for(let i = 0; i<string.length; i++){
+
+				for (let i = 0; i < string.length; i++) {
 					let numChar = ((string.charCodeAt(i) + 65 - offset) % 26) + 65;
-					if(string[i] == " "){
+					if (string[i] == " ") {
 						msnDecode += string[i];
 					} else {
 						msnDecode += String.fromCharCode(numChar);
@@ -82,5 +66,3 @@ window.cipher = {
 		return newObject;
 	}
 };
-
-//window.document.all[7].value
